d9efe1ccd2ac89920c62c9b782aa7ae66b536589
paul pettipas <paul.pettipas@gmail.com>
2014-12-15 13:12:57 -0400

    simple character selector

---
+++ Assets/Art/Selector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selector : MonoBehaviour {
+
+	public void Update(){
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray,out hit) && 
+		   Input.GetMouseButtonUp(0))
+		{
+
+			Debug.Log (hit.transform.name);
+		}
+
+		
+	}
+
+}
