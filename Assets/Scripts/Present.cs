071029bd283edf5a2a483b0d7e2b89fbd0568468
paul pettipas <paul.pettipas@gmail.com>
2014-12-15 15:06:18 -0400

    prents dissapear

--- Assets/Scripts/Present.cs
+++ Assets/Scripts/Present.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Present : MonoBehaviour {
 
-	// Use this for initialization
+
 	void Start () {
-	
+		StartCoroutine(WaitAndDestroy());
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	IEnumerator WaitAndDestroy(){
+		yield return new WaitForSeconds(10.0f);
+		Destroy(gameObject);
 	}
 }
